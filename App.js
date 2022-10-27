6c1987f50695aef87ebabf4db5b82c2bf7b8b69d
santragun <48906270+santragun@users.noreply.github.com>
2022-10-27 15:10:27 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LoginControl from "./components/LoginControl";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src=logo  alt="logo" />
+        <LoginControl />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
